5d3ee87fd4fe288501036441412e5ceced722bc7
Sebastián <sebastian.gonzalez.cass@usach.cl>
2025-01-16 00:26:23 -0300

    fix: adaptar app a docker

--- docker.sh
+++ docker.sh
@@ -11,19 +11,19 @@ docker build -t sebacassone/gateway-server .
 docker push sebacassone/gateway-server
 
 cd ../income-server
-mvn clean install -DskipTests=True
-docker build -t sebacassone/prueba-server .
-docker push sebacassone/prueba-server
+./gradlew clean build -x test
+docker build -t sebacassone/income-server .
+docker push sebacassone/income-server
 
 cd ../management-server
 ./gradlew clean build -x test
-docker build -t sebacassone/addresses-server .
-docker push sebacassone/addresses-server
+docker build -t sebacassone/management-server .
+docker push sebacassone/management-server
 
 cd ../report-server
 ./gradlew clean build -x test
-docker build -t sebacassone/debts-server .
-docker push sebacassone/debts-server
+docker build -t sebacassone/report-server .
+docker push sebacassone/report-server
 
 # cd ../../Monolítica/frontend/
 # docker build --build-arg VITE_PAYROLL_BACKEND_SERVER=192.168.49.2:30000 -t sebacassone/frontend-server .
